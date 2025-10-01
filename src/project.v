764ffc7eb6cfe5847ee6e3bddcc2a44bb5ecbdbe
davvdf <david88gao@gmail.com>
2025-09-30 21:41:18 -0400

    durr

--- src/project.v
+++ src/project.v
@@ -17,7 +17,6 @@ module tt_um_tri_state_counter (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
   tri_counter tri_counter_inst(
--- src/tri_counter.v
+++ src/tri_counter.v
@@ -8,17 +8,17 @@ module tri_counter #(
     input wire [DATA_WIDTH-1:0] d_in,
     output wire [DATA_WIDTH-1:0] d_out
 );
-    reg out;
+    reg [DATA_WIDTH-1:0] count;
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            out <= '0;
+            count <= '0;
         end else begin
             if (ld) begin
-                out <= d_in;
+                count <= d_in;
             end else begin
-                out <= out + 1;
+                count <= count + 1;
             end
         end
     end
-    assign d_out = en ? out : 'z;
+    assign d_out = en ? count : 'z;
 endmodule
