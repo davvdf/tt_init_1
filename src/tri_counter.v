c800f038e174f1c0d7b75732d5cfaaf8f33f0b4d
davvdf <david88gao@gmail.com>
2025-09-30 21:12:48 -0400

    asdf

--- src/tri_counter.v
+++ src/tri_counter.v
@@ -8,12 +8,9 @@ module tri_counter #(
     input wire [DATA_WIDTH-1:0] d_in,
     output wire [DATA_WIDTH-1:0] d_out
 );
-    wire buff;
     reg out;
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            buff <= '0;
-            d_out <= '0;
             out <= '0;
         end else begin
             if (ld) begin
